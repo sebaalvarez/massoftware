ec6fe923394d15a7070060b0bd4a66ff4e3a62ca
Diego Pablo Mansilla <diegopablomansilla@gmail.com>
2017-08-22 22:01:30 -0300

    terminando menu contabilidad general

--- sql/puesta_a_punto.sql
+++ sql/puesta_a_punto.sql
@@ -1,4 +1,14 @@
-ALTER TABLE CentrosDeCostoContable  
+ALTER TABLE [dbo].[CentrosDeCostoContable]  
 	ADD CONSTRAINT fk_EjerciciosContables  
 		FOREIGN KEY (Ejercicio)                              
 		REFERENCES EjerciciosContables(Ejercicio);
+		
+  
+ALTER TABLE [dbo].[SSECUR_User] ADD DEFAULT_EJERCICIO_CONTABLE int NULL;  
+
+ALTER TABLE [dbo].[SSECUR_User] 
+	ADD CONSTRAINT fk_EjerciciosContables_SSECUR_User  
+		FOREIGN KEY (DEFAULT_EJERCICIO_CONTABLE)                              
+		REFERENCES EjerciciosContables(Ejercicio);
+
+		
